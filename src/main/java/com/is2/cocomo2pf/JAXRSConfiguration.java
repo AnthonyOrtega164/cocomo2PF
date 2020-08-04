781c4a5bcdf4b856f8b79002252c623f054d4849
AnthonyDOM <anthonyortega164@gmail.com>
2020-08-04 18:20:37 -0500

    Initial commit

---
+++ src/main/java/com/is2/cocomo2pf/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.is2.cocomo2pf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
